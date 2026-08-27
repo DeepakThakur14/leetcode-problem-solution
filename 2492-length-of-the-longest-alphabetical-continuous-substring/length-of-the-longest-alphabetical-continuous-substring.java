183865e9f034d6e8e726abfcfa9fcc751cbce47c
Deepak Thakur <deepak.gla_cs24@gla.ac.in>
2026-08-27 09:18:22 +0530

    Time: 12 ms (76.09%) | Memory: 47.6 MB (25.90%) - LeetSync

---
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestContinuousSubstring(String s) {
+        int max = 1;
+        int count = 1;
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i)-s.charAt(i-1)==1){
+                count++;
+            }
+            else{
+                count =1;
+            }
+            if(count>max){
+                max = count;
+            }
+        }
+        return max;
+    }
+}
